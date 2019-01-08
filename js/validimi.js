5578cb48d92f5f599fcf9bd008201c5b9b425564
donidemolli <44557862+donidemolli@users.noreply.github.com>
2019-01-08 04:03:43 +0100

    Add files via upload

--- js/validimi.js
+++ js/validimi.js
@@ -7,37 +7,49 @@ function validimi()
    
     if (name.value == "")                                  
     { 
-        window.alert("Ju lutem shkruani emrin dhe mbiemri"); 
+        window.alert("Ju lutem shkruani Emrin dhe Mbiemrin."); 
         name.focus(); 
         return false; 
     } 
-   
-    if (dscp.value == "")                               
-    { 
-        window.alert("Please enter your address."); 
-        name.focus(); 
-        return false; 
-    } 
-       
+         
     if (email.value == "")                                   
     { 
-        window.alert("Please enter a valid e-mail address."); 
+        window.alert("Ju lutem shkruani një e-mail adresë."); 
         email.focus(); 
         return false; 
     } 
    
     if (email.value.indexOf("@", 0) < 0)                 
     { 
-        window.alert("Please enter a valid e-mail address."); 
+        window.alert("Ju lutem shkruani një e-mail adresë valide"); 
         email.focus(); 
         return false; 
     } 
    
     if (email.value.indexOf(".", 0) < 0)                 
     { 
-        window.alert("Please enter a valid e-mail address."); 
+        window.alert("Ju lutem shkruani një e-mail adresë valide"); 
         email.focus(); 
         return false; 
     }    
+    
+    if (dscp.value == "")                               
+    { 
+        window.alert("Nuk mund të dërgoni mesazh të zbrazët."); 
+        name.focus(); 
+        return false; 
+    } 
     return true; 
+}
+
+
+function validimi2(){
+    var email = document.forms["forma2"]["email"];
+    if (email.value == "")                                   
+    { 
+        window.alert("Ju lutem shkruani një e-mail adresë."); 
+        email.focus(); 
+        return false; 
+    } 
+    return true;
 }
